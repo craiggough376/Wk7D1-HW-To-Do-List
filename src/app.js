00a212eb8f4bbe36c7962a7eac9230b368a24320
craiggough376 <craig_gough_376@hotmail.com>
2019-06-17 20:55:18 +0100

    extensions completed

--- src/app.js
+++ src/app.js
@@ -4,13 +4,21 @@ document.addEventListener('DOMContentLoaded', () => {
   const instance = new Vue({
     el: "#app",
     data: {
-      todos: ["Buy Shopping", "Clean Car", "Car's MOT"],
-      newItem: ""
+      todos: [
+      {name: "Buy Shopping", priority: true},
+      {name: "Clean Car", priority: false},
+      {name: "Car's MOT", priority: true}
+    ],
+      newItem: "",
+      priority: false
     },
     methods: {
       saveNewItem: function() {
-        this.todos.push(this.newItem);
+        this.todos.push({
+        name: this.newItem,
+        priority: this.priority});
         this.newItem = "";
+        this.priority = false;
       }
     }
   });
